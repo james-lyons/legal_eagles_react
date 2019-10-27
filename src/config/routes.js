3aa67bc990ccb495aee02f9eb5ae93e2f1b6e11a
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-10-26 21:37:09 -0700

    routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -44,7 +44,7 @@ export default withRouter(() => {
         <Switch>
             <Route exact path = '/' component = { Home } />
             <PrivateRoute path = '/attorney_search' component={ AttorneySearch } />
-            <PrivateRoute path = '/attorney/:attorney_url' component={AttorneyPublicProfile } { ...this.props } />
+            <PrivateRoute path = '/attorney/:attorney_url' render={ props => <AttorneyPublicProfile {...props} /> }/>
             <AttorneyRoute path = '/attorney_profile' component = { AttorneyPrivateProfile } />
             <ClientRoute path = '/client_profile' component = { ClientProfile } />
             <Route component={ redirectToHome }/>
