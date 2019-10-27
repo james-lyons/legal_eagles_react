5a1ee919c1d6aee734f48b2308f24b0f47b3a1fa
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-10-26 21:12:30 -0700

    makes app functional component because i said so

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import Routes from './config/routes';
 import NavBar from './components/Layout/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <Routes />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes />
+    </div>
+  );
 }
 
 export default withRouter(App);
